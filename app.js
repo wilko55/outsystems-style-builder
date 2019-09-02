cfe3c2c21e268244fe0441882c222faeb97133b2
Andy <andy@mediasuite.co.nz>
2019-09-03 10:45:43 +1200

    Made more generic to be used per env

--- app.js
+++ app.js
@@ -7,19 +7,16 @@ const port = 3001;
 const defaultTargetBranch = 'develop';
 const outsystemsStylesRepo = 'https://github.com/wilko55/outsystems-styles';
 
-const envs = [
-  'develop',
-];
+const path = 'styles';
 
 shell.exec(`./initialise-outsystems-styles.sh ${outsystemsStylesRepo}`);
 
-envs.forEach(env => {
-  app.use(`/${env}/`, express.static(`public/${env}`));
-});
+app.use(`/${path}/`, express.static(`styles`));
 
 app.use(bodyParser.json())
 
 app.post('/refresh', (req, res) => {
+  // set up webhook for correct env remote
   const refParts = req.body.ref.split('/');
   let targetBranch = refParts[refParts.length];
   if (!targetBranch) {
--- build-styles.sh
+++ build-styles.sh
@@ -8,10 +8,10 @@ echo "Pulling latest sass files from origin/$develop"
 git reset --hard origin/$1
 git pull origin $1
 
-echo "Removing old styles in public/$1"
-rm -rf ..public/$1
+echo "Removing old styles in /styles"
+rm -rf ..styles/
 
-echo "Building styles to public/$1"
-../node_modules/node-sass/bin/node-sass . -o ../public/$1
-# build from sass to correct public dic
+echo "Building styles to /styles"
+../node_modules/node-sass/bin/node-sass . -o ../styles/
+# build from sass to correct styles dic
 # check app serves static assets correctly
