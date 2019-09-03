7e3bb23d7e50f6cbccd48c38f5f2687033e7422b
Andy <andy@mediasuite.co.nz>
2019-09-03 16:37:27 +1200

    Updated readme

--- build-styles.sh
+++ build-styles.sh
@@ -2,9 +2,9 @@
 echo "Building on branch $1"
 
 cd outsystems-styles
-echo "Checking out branch $develop"
+echo "Checking out branch $1"
 git checkout $1
-echo "Pulling latest sass files from origin/$develop"
+echo "Pulling latest sass files from origin/$1"
 git fetch origin $1
 git reset --hard origin/$1
 
@@ -13,5 +13,3 @@ rm -rf ..styles/
 
 echo "Building styles to /styles"
 ../node_modules/node-sass/bin/node-sass . -o ../styles/
-# build from sass to correct styles dic
-# check app serves static assets correctly
